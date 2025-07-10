61c8a4b1ddcafbc4dd119697c47b8712e2e42fc2
me32face <me32face@gmail.com>
2025-07-11 01:37:25 +0530

    Update main files and config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import MenuHeader from './components/MenuHeader';
+import MenuItemsGrid from './components/MenuItemsGrid';
+import ContactSection from './components/ContactSection';
+import BaseFooter from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <MenuHeader />
+      <MenuItemsGrid />
+      <ContactSection />
+      <BaseFooter/>
+    </>
   );
 }
 
